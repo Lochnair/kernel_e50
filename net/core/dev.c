9471eb6bb0fd776532072708ff225d9f863bc3c7
Xiaoping Fan <xfan@codeaurora.org>
2016-07-15 16:41:03 -0700

    CHROMIUM: net: patch kernel to support SW Shortcut Forwarding Engine (SFE)
    1. To support SFE
    1.1 Added a hook in netif_receive_skb() to pass packet to SFE
        for acceleration.
    1.2 Added a field fast_forwarded in sk_buff to skip some heavy
        processing in kernel.
    
    2. To support multiple listeners of connection event
    2.1 Multiple modules need to listen on connection events.
        "net->ct.nf_conntrack_event_cb" can support only one listener,
        so we replaced it with a notification chain "net->ct.nf_conntrack_chain"
        to support multiple listeners.
    
    3. To update stats on bridge interface
    3.1 Added a new function br_dev_update_stats in Linux bridge.
        Fast-classifier has an option to skip bridge processing for
        routed packets. So Fast-classifier need this function to update
        stats on bridge interface.
    
    BUG=chrome-os-partner:56410
    TEST=DK04. In addition, ran throughput tests on
    a wired client with multiple streams.
    
    Change-Id: I9aa4db3bdfe55f1d8e870c03010e76512570a48d
    Signed-off-by: Xiaoping Fan <xfan@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/361050
    Commit-Ready: Kishan Kunduru <kkunduru@chromium.org>
    Tested-by: Kishan Kunduru <kkunduru@chromium.org>
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-by: Grant Grundler <grundler@chromium.org>
    Reviewed-by: Kevin Hayes <kevinhayes@google.com>
    Reviewed-by: Kishan Kunduru <kkunduru@chromium.org>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2602,13 +2602,14 @@ int dev_hard_start_xmit(struct sk_buff *skb, struct net_device *dev,
 			}
 		}
 
+		if (!skb->fast_forwarded)
 #if defined(CONFIG_IMQ) || defined(CONFIG_IMQ_MODULE)
-		if (!list_empty(&ptype_all) &&
-					!(skb->imq_flags & IMQ_F_ENQUEUE))
+			if (!list_empty(&ptype_all) &&
+						!(skb->imq_flags & IMQ_F_ENQUEUE))
 #else
- 		if (!list_empty(&ptype_all))
+ 			if (!list_empty(&ptype_all))
 #endif
- 			dev_queue_xmit_nit(skb, dev);
+ 				dev_queue_xmit_nit(skb, dev);
  
 
 		skb_len = skb->len;
@@ -3446,6 +3447,9 @@ void netdev_rx_handler_unregister(struct net_device *dev)
 }
 EXPORT_SYMBOL_GPL(netdev_rx_handler_unregister);
 
+int (*fast_nat_recv)(struct sk_buff *skb) __rcu __read_mostly;
+EXPORT_SYMBOL_GPL(fast_nat_recv);
+
 /*
  * Limit the use of PFMEMALLOC reserves to those protocols that implement
  * the special handling of PFMEMALLOC skbs.
@@ -3473,6 +3477,7 @@ static int __netif_receive_skb_core(struct sk_buff *skb, bool pfmemalloc)
 	bool deliver_exact = false;
 	int ret = NET_RX_DROP;
 	__be16 type;
+	int (*fast_recv)(struct sk_buff *skb);
 
 	net_timestamp_check(!netdev_tstamp_prequeue, skb);
 
@@ -3501,6 +3506,12 @@ another_round:
 		skb = vlan_untag(skb);
 		if (unlikely(!skb))
 			goto out;
+	}
+
+	fast_recv = rcu_dereference(fast_nat_recv);
+	if (fast_recv && fast_recv(skb)) {
+		ret = NET_RX_SUCCESS;
+		goto out;
 	}
 
 #ifdef CONFIG_NET_CLS_ACT
